c46413165d1f7ab7d425bae923d7b9f16e89a7a1
Miguel Medina <miguelmedinareina@gmail.com>
2025-03-04 18:14:16 +0100

    change

--- VIX.py
+++ VIX.py
@@ -20,7 +20,7 @@ data= requests.get('https://www.alphavantage.co/query', params=params).json()
 
 print(data)
 # ------------------------
-# -------example 3------------
+# -------example 3 ------------
 
 
 
